5bf421e5c609892e74bcc487b74aeaa457ea0494
mendzmartin <chmartinmendez@gmail.com>
2024-07-05 17:15:36 -0300

    adding simulation from cluster

--- 030_experimentation/030_test/coulomb_potential_2D_parameter_variation/030_src/run.jl
+++ 030_experimentation/030_test/coulomb_potential_2D_parameter_variation/030_src/run.jl
@@ -1,6 +1,6 @@
 begin
     develop_package = false
-    computer = "pcfamaf"
+    computer = "ccad"
 
     if computer == "notebook"
         path_repo = "/home/mendez/github_repositories/my_repositories/"
@@ -28,5 +28,5 @@ begin
     using Revise;
     using FEMTISE;
 
-    run_default_eigen_problem(set_type_potential("/home/martin/github_repositories/my_repositories/FEMTISE_TUTORIAL/030_experimentation/030_test/coulomb_potential_2D_parameter_variation/020_input/input"))
+    run_default_eigen_problem(set_type_potential("/home/martinmendez/github_repositories/my_repositories/FEMTISE_TUTORIAL/030_experimentation/030_test/coulomb_potential_2D_parameter_variation/020_input/input"))
 end
