06e9cee9e5537de9de0bfa2555ee7d2acbe71d1b
mendzmartin <chmartinmendez@gmail.com>
2024-07-10 13:56:08 -0300

    adding Helium atom simulation from cluster

--- 030_experimentation/030_test/coulomb_potential_2D_parameter_variation/050_analysis/adhoc_potential_function.jl
+++ 030_experimentation/030_test/coulomb_potential_2D_parameter_variation/050_analysis/adhoc_potential_function.jl
@@ -17,3 +17,32 @@ function effective_electronic_potential_2D(x,params::Tuple)
     qho_2D_params=tuple(1.0,0.0,0.0)
     return qho_2D(x,qho_2D_params)+electronic_coulomb_potential_interaction_2D(x,params)
 end
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++
+measure_length(DOF1,DOF2)=abs(DOF2-DOF1)
+
+effective_interaction(absx) = exp(absx^2)*erfc(absx)
+asymptotical_interaction(absx) = (1/sqrt(π))*(1/absx)
+
+function avoided_divergence(absx,b)
+    absx > 25.7 ? func=asymptotical_interaction(absx+b) : func=effective_interaction(absx+b)
+    return func
+end
+
+function reduced_confinement_function(DOF1,DOF2,confinement_length,Yukawa_length,atomic_number)
+    a=atomic_number*π*sqrt(π)*0.5*(1.0/confinement_length);
+    b=confinement_length*0.5*(1.0/abs(Yukawa_length));
+    coordinate=measure_length(DOF1,DOF2)*(1.0/confinement_length);
+    return a*exp((-2.0*coordinate+b)*b)*avoided_divergence(coordinate,b)
+end
+
+export effective_Yukawa_potential_2e
+function effective_Yukawa_potential_2e(x,params::Tuple)
+    confinement_length,Yukawa_length,nuclei_coord,nuclei_mass,atomic_number=params;
+    # return (reduced_confinement_function(x[1],x[2],confinement_length,Yukawa_length,1.0)
+    # -reduced_confinement_function(x[1],nuclei_coord,confinement_length,Yukawa_length,atomic_number)
+    # -reduced_confinement_function(nuclei_coord,x[2],confinement_length,Yukawa_length,atomic_number))
+    return (-reduced_confinement_function(x[1],nuclei_coord,confinement_length,Yukawa_length,atomic_number)
+    -reduced_confinement_function(nuclei_coord,x[2],confinement_length,Yukawa_length,atomic_number))
+end
